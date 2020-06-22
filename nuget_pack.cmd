eef8c4506d89edb9ca758ac9269a5e28634560a8
patrick <patrick.borek@software-arenal.es>
2020-06-22 12:24:20 +0200

    updated nuget metadata, removed nuspec file

--- nuget_pack.cmd
+++ nuget_pack.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-dotnet pack src\Markdown.Xaml\MarkdownFlowDocument.csproj --configuration Release /property:PackageVersion=1.2.0 /p:PackageOutputPath=. /p:Authors=Author
+dotnet pack src\Markdown.Xaml\MarkdownFlowDocument.csproj --configuration Release /property:PackageVersion=1.6.1 /p:PackageOutputPath=.
 if errorlevel 1 pause
--- nuget_push.cmd
+++ nuget_push.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-dotnet nuget push src/Markdown.Xaml/MarkdownFlowDocument.1.2.0.nupkg --api-key %API_KEY% --source https://api.nuget.org/v3/index.json
+dotnet nuget push src/Markdown.Xaml/MarkdownFlowDocument.1.6.1.nupkg --api-key %API_KEY% --source https://api.nuget.org/v3/index.json
 if errorlevel 1 pause
